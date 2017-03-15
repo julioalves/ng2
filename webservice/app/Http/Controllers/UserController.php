455e5325c3bec928e9e46321257a267780903db9
Paola Hoff Alves <Paola Hoff Alves>
2017-03-15 18:57:38 -0300

    database

--- webservice/app/Http/Controllers/UserController.php
+++ webservice/app/Http/Controllers/UserController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        header('Access-Control-Allow-Origin: *');
+        $data = $request->all();
+
+        $data['api_token'] = str_random(60);
+
+        if (!User::whereEmail($data['email'])->count())
+        {
+            $data['password'] = bcrypt($data['password']);
+            $user = User::create($data);
+            return response()->json(['data'=> $user, 'status'=>true]);
+        }
+
+        return response()->json(['data'=> 'This email is already taken', 'status'=>false]);
+    }
 }
--- webservice/database/seeds/DatabaseSeeder.php
+++ webservice/database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\News;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        for ($i=0;$i < 10;$i++)
+        {
+            News::create(
+                [
+                    "title"=> "noticia $i",
+                    "description" => 'description $i',
+                    "image" => 'image1',
+                ]
+            );
+
+        }
+
     }
 }
